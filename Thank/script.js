2098343625a5f2ed342fac69162f8564dfac1dec
imamfahr <imamfahr@gmail.com>
2020-08-23 20:03:51 +0800

    thank you robot

--- Thank/script.js
+++ Thank/script.js
@@ -2,5 +2,8 @@ let namaPembeli = prompt('siapa nama kamu ?');
 let produkDibeli = prompt('barang apa yang kamu beli?');
 
 function terimaKasih(){
-    console.log(`terimakasih ${namaPembeli} telah memberli produk ${produkDibeli}`)
+    alert(`terimakasih ${namaPembeli} telah memberli produk ${produkDibeli}`);
+    console.log(`terimakasih ${namaPembeli} telah memberli produk ${produkDibeli}`);
 }
+
+terimaKasih()
