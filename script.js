9241ed206f1c8e5ed95d8dd34eaad8c148d74d81
imamfahr <imamfahr@gmail.com>
2020-08-23 17:04:22 +0800

    add new html and js

---
+++ script.js
@@ -0,0 +1,6 @@
+let namaPembeli = prompt('siapa nama kamu ?');
+let produkDibeli = prompt('barang apa yang kamu beli?');
+
+function terimaKasih(){
+    console.log(`terimakasih ${namaPembeli} telah memberli produk ${produkDibeli}`)
+}
