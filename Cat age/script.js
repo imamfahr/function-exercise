079074d067c19952d16c22d1e119c343aa3ed496
imamfahr <imamfahr@gmail.com>
2020-08-23 21:02:54 +0800

    add cat age converter

---
+++ Cat age/script.js
@@ -0,0 +1,21 @@
+let catAge = prompt('please input your cat Age in year(s)');
+
+function catToHumanCoverter(catAge){
+    if(catAge<=1){
+        return catAge*15;
+    }
+    else if(1<catAge && catAge<=2){
+        return catAge*12;
+    }
+    else if(2<catAge && catAge<=3){
+        return catAge*8;
+    }
+    else if(catAge>3){
+        return 24+((catAge-2)*4);
+    }
+    else{
+        return "please input correct value"
+    }
+} 
+
+alert(`your cat age is equal to ${catToHumanCoverter(catAge)}`);
